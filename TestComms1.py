58cc9da998500845a94963338b0220769f6d96b7
jPruim <44886135+jPruim@users.noreply.github.com>
2019-01-11 14:58:06 -0500

    Add files via upload

---
+++ TestComms1.py
@@ -0,0 +1,18 @@
+# test Comms made for Calvin Mars Rover 2018-19
+# by Jason Pruim and Joel Muyskens based on digi.xBee github
+# imports other tests from directory
+
+import TestSend1
+import TestReceive2
+import time
+
+# if main program then define defaults and run 2 tests,
+if __name__ ==__main__:
+    # values to be changed on different computers
+    port = "COM3"
+    baud = 19200
+    print("Start tests")
+    time.sleep(3)
+    test_send1(port, baud)
+    test_receive2(port,baud)
+    print("Tests Completed")
